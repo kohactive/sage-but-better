16fdefd01895c797ccb955faa93f0fc1273b7d9f
Andy Richardson <andy@kohactive.com>
2017-01-24 16:46:08 -0500

    General Updates
    * remove coffee
    * add options page support

--- wp-content/themes/sage-but-better/functions.php
+++ wp-content/themes/sage-but-better/functions.php
@@ -26,3 +26,13 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+if( function_exists('acf_add_options_page') ) {
+  acf_add_options_page();
+}
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
